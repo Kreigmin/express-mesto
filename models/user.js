05e87926fae696fec74bcac1d5a8753c56bc456e
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-15 18:25:34 +0300

    feat: add own method to model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const validator = require("validator");
 
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -32,9 +34,23 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
+    // required: true,
     minlength: 6,
   },
 });
+
+userSchema.statics.findUserbyCredentials = function (email, password) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Unauthorized"));
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Unauthorized"));
+      }
+      return user;
+    });
+  });
+};
 
 module.exports = mongoose.model("user", userSchema);
