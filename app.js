4036ac37804bb6fbe851653226c6a6af67dc7b53
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-18 22:04:40 +0300

    feat: add global error handler and middleware for celebrate errors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const helmet = require("helmet");
+const { errors } = require("celebrate");
 const userRoutes = require("./routes/users");
 const cardRoutes = require("./routes/cards");
 const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
 
+const BASE_ERROR_CODE = 500;
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -25,6 +27,17 @@ app.use(auth);
 app.use(userRoutes);
 
 app.use(cardRoutes);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = BASE_ERROR_CODE, message } = err;
+  res.status(statusCode).send({
+    message:
+      statusCode === BASE_ERROR_CODE ? "На сервере произошла ошибка" : message,
+  });
+});
 
 app.use((req, res) => {
   const NOT_FOUND_ERROR_CODE = 404;
