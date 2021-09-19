92e119dcfd80735f4be53cf53f5aa9d19c0c1490
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-19 16:17:36 +0300

    feat: describe models for Joi validation

--- routes/cards.js
+++ routes/cards.js
@@ -1,3 +1,5 @@
+/* eslint-disable comma-dangle */
+const { Joi, celebrate } = require("celebrate");
 const express = require("express");
 
 const router = express.Router();
@@ -10,10 +12,56 @@ const {
   dislikeCard,
 } = require("../controllers/cards");
 
-router.get("/cards", getAllCards);
-router.post("/cards", createCard);
-router.delete("/cards/:cardId", deleteCard);
-router.put("/cards/:cardId/likes", likeCard);
-router.delete("/cards/:cardId/likes", dislikeCard);
+router.get(
+  "/cards",
+  celebrate({
+    headers: Joi.object()
+      .keys({
+        "Content-Type": Joi.string().required,
+      })
+      .unknown(true),
+  }),
+  getAllCards
+);
+
+router.post(
+  "/cards",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required(),
+    }),
+  }),
+  createCard
+);
+
+router.delete(
+  "/cards/:cardId",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+  }),
+  deleteCard
+);
+
+router.put(
+  "/cards/:cardId/likes",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+  }),
+  likeCard
+);
+router.delete(
+  "/cards/:cardId/likes",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+  }),
+  dislikeCard
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
+/* eslint-disable comma-dangle */
 const express = require("express");
+
+const { celebrate, Joi } = require("celebrate");
 
 const router = express.Router();
 const {
@@ -9,14 +12,43 @@ const {
   updateAvatar,
 } = require("../controllers/users");
 
-router.post("/users", createUser);
+router.post(
+  "/users",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6),
+    }),
+  }),
+  createUser
+);
 
 router.get("/users", getAllUsers);
 
 router.get("/users/me", getUser);
 
-router.patch("/users/me", updateUserInfo);
+router.patch(
+  "/users/me",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUserInfo
+);
 
-router.patch("/users/me/avatar", updateAvatar);
+router.patch(
+  "/users/me/avatar",
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string(),
+    }),
+  }),
+  updateAvatar
+);
 
 module.exports = router;
